b99831bf13dd2821ca360365accb54a40ef13a60
rivesuvacrispa <vova.cpeda@yandex.ru>
2026-05-26 23:39:12 +0300

    feature: spellcasting, controls manager

--- Assets/Scripts/Effects/SpellBall.cs
+++ Assets/Scripts/Effects/SpellBall.cs
@@ -1,35 +1,74 @@
-﻿using UnityEngine;
-using Random = UnityEngine.Random;
+﻿using System.Collections;
+using UnityEngine;
 
 namespace Effects
 {
     [RequireComponent(typeof(Rigidbody))]
     public class SpellBall : MonoBehaviour
     {
-        [SerializeField] private Rigidbody rb;
-
+        [Header("References")]
+        [SerializeField] private ParticleSystem particles;
+        
+        [Header("Settings")]
+        [SerializeField] private float minVelocityToDespawn = 4f;
+        [SerializeField] private int collisionsToDespawn = 8;
+        
+        private Rigidbody rb;
+        private bool isDespawning = false;
+        private bool isSpawning = true;
+        private int collisionCounter = 0;
         public delegate void SpellBallCollisionEvent(SpellBall ball, Collision collision);
         public static event SpellBallCollisionEvent OnMirrorCollisionGlobal;
         private static int wallsLayer;
-        
+        public Rigidbody Rigidbody => rb;
+
+
         
         private void Awake()
         {
             wallsLayer = LayerMask.NameToLayer("Walls");
             rb = GetComponent<Rigidbody>();
+            StartCoroutine(SpawnRoutine());
         }
-
-        private void Start()
-        {
-            rb.AddForce(Random.insideUnitSphere.normalized * 100);
-        }
-
+        
         private void OnCollisionEnter(Collision collision)
         {
-            rb.linearVelocity *= 1.25f;
+            if (isDespawning || isSpawning) return;
             
+            rb.linearVelocity *= 1.25f;
+            collisionCounter++;
+
             if (collision.gameObject.layer == wallsLayer)
                 OnMirrorCollisionGlobal?.Invoke(this, collision);
+            
+            if (collisionCounter >= collisionsToDespawn)
+                Despawn();
+        }
+
+        private void Update()
+        {
+            if (rb.linearVelocity.magnitude < minVelocityToDespawn)
+                Despawn();
+        }
+
+        private void Despawn()
+        {
+            if (isDespawning || isSpawning) return;
+            StartCoroutine(DespawnRoutine());
+        }
+
+        private IEnumerator DespawnRoutine()
+        {
+            isDespawning = true;
+            particles.Stop();
+            yield return new WaitUntil(() => !particles.isPlaying);
+            Destroy(gameObject);
+        }
+
+        private IEnumerator SpawnRoutine()
+        {
+            yield return new WaitForSeconds(0.5f);
+            isSpawning = false;
         }
     }
 }
